994aec58c40f7970905aff017c15ee593325b416
cjy-dell <554951030@qq.com>
2021-12-03 01:16:30 +0800

    ss

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+function App() {
+  return (
+    <div className="App">
+      react app
+      <p key='ppp1' style={{color: 'red'}}>p标签</p>
+      <span>这是span标签</span>
+    </div>
+  );
+}
+
+export default App;
